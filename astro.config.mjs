96243e4052f7c736054de709e38fd24941e49783
Eduardo Torres Davila <etdavila10@gmail.com>
2025-03-25 22:35:59 -0500

    fixing links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://etdavila10.github.io",
+  base: "/FrontendMentors",
   vite: {
     plugins: [tailwindcss()],
   },
